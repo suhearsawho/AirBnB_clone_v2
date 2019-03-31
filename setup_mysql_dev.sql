54433a2ef64fabda9cca1eb04adbf1616bb4f484
stak21 <stakashima21@gmail.com>
2019-03-31 00:56:47 +0000

    Added comments

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,3 +1,5 @@
+-- Creates a database and a developer user granting specific permissions on
+-- certain databases
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 USE hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
