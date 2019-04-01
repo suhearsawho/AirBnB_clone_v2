e348089b0bce083d67f8345a0cbb9b28bfd2c94d
susansu <susansu.software@gmail.com>
2019-04-01 20:32:53 +0000

    changes to base_model

--- models/base_model.py
+++ models/base_model.py
@@ -39,7 +39,6 @@ class BaseModel:
         else:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
-            models.storage.new(self)
 
     def __str__(self):
         """returns a string
@@ -58,6 +57,7 @@ class BaseModel:
         """updates the public instance attribute updated_at to current
         """
         self.updated_at = datetime.now()
+        models.storage.new(self)
         models.storage.save()
 
     def to_dict(self):
@@ -66,7 +66,14 @@ class BaseModel:
             returns a dictionary of all the key values in __dict__
         """
         my_dict = dict(self.__dict__)
+        if '_sa_instance_state' in my_dict().keys():
+            del my_dict['_sa_instance_state']
+
         my_dict["__class__"] = str(type(self).__name__)
         my_dict["created_at"] = self.created_at.isoformat()
         my_dict["updated_at"] = self.updated_at.isoformat()
         return my_dict
+
+    def delete(self):
+        """Deletes the current instance from storage"""
+        models.storage.delete(self)
