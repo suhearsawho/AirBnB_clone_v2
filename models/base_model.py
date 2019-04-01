c8e052b94cdf0bf1bfe369bbd9a6c6ceff37ed67
stak21 <stakashima21@gmail.com>
2019-04-01 22:25:14 +0000

    E: couldnt iterate though dict F: removed unnecessary parenthesis

--- models/base_model.py
+++ models/base_model.py
@@ -67,7 +67,7 @@ class BaseModel:
             returns a dictionary of all the key values in __dict__
         """
         my_dict = dict(self.__dict__)
-        if '_sa_instance_state' in my_dict().keys():
+        if '_sa_instance_state' in my_dict.keys():
             del my_dict['_sa_instance_state']
 
         my_dict["__class__"] = str(type(self).__name__)
