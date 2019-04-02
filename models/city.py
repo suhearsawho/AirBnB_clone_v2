2271b4ca5de6d443d4743933d9c8e4744107ed2e
stak21 <stakashima21@gmail.com>
2019-04-02 18:13:19 +0000

    Imported relationship; created a relationship with Place

--- models/city.py
+++ models/city.py
@@ -3,6 +3,7 @@
 import sqlalchemy
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
@@ -15,3 +16,5 @@ class City(BaseModel, Base):
     __tablename__ = 'cities'
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+
+    places = relationship("Place", cascade="all, delete", backref="cities")
