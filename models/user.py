191e62228c1fe3cdaec212cf354066a68286f7ab
susansu <susansu.software@gmail.com>
2019-04-02 17:20:27 +0000

    Implements table schema for MySQL database

--- models/user.py
+++ models/user.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python3
 """This is the user class"""
-from models.base_model import BaseModel
+import sqlalchemy
+
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, ForeignKey, String
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """This is the class for user
     Attributes:
+        __tablename__: table name in MySQL
         email: email address
         password: password for you login
         first_name: first name
         last_name: last name
     """
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+    __tablename__ = 'users'
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
