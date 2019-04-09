e65566cc2a3d141c140645f11102711b8d40b57a
susansu <susansu.software@gmail.com>
2019-04-09 05:23:28 +0000

    deploy static

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,16 +1,57 @@
 #!/usr/bin/python3
 """Deploy function"""
 from fabric.api import *
+import datetime
+import os
 
-do_pack = __import__('1-pack_web_static').do_pack
-do_deploy = __import__('2-do_deploy_web_static').do_deploy
+env.hosts = ['35.185.66.253', '35.229.43.253']
+
+
+@runs_once
+def do_pack():
+    """This function generates a .tgz archive from contents of
+    web_static folder of AirBnb Clone repo
+    """
+    now = datetime.datetime.now()
+    path = 'versions/web_static_' +\
+           '{}{}{}{}{}{}'.format(now.year, now.month,
+                                 now.day, now.hour,
+                                 now.minute, now.second) + '.tgz'
+
+    local('mkdir -p versions')
+    success = local('tar -cvzf {:s} web_static'.format(path), capture=True)
+    if success.return_code == 0:
+        return path
+
+
+def do_deploy(archive_path):
+    """Distributes an archive to web servers"""
+    if os.path.isfile(str(archive_path)) is 0:
+        return False
+
+    path = '/data/web_static/releases/{}/'.format(archive_path[9:-4])
+    try:
+        # Upload archive to /tmp/ directory of the web server
+        put(archive_path, '/tmp/')
+        # Uncompress the archive to the folder
+        run('mkdir -p {}'.format(path))
+        run('tar -xzf /tmp/{} -C {}'.format(archive_path[9:], path))
+        run('rm /tmp/{}'.format(archive_path[9:]))
+        run('mv {}web_static/* {}'.format(path, path))
+        run('rm -rf {}web_static'.format(path))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {} /data/web_static/current'.format(path))
+    except:
+        return False
+    else:
+        print('New version deployed!')
+        return True
 
 
 def deploy():
     """Fabric script that creates and distributes an archive to web servers"""
     archive = do_pack()
-    if archive == None:
+    if archive is None:
         return False
 
-    env.hosts = ['35.185.66.253', '35.229.43.253']
     return do_deploy(archive)
