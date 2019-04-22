ec19ca72b5d3e3f86cda13a71c68b9097c06a46b
susansu <susansu.software@gmail.com>
2019-04-22 17:51:31 +0000

    Added comments

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""This script runs a Flask application"""
-
+"""Starts a flask web application"""
 from flask import Flask
+
 app = Flask(__name__)
 
 
@@ -9,6 +9,12 @@ app = Flask(__name__)
 def index():
     """Defines the index page"""
     return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Defines the hbnb page"""
+    return 'HBNB'
 
 
 if __name__ == '__main__':
