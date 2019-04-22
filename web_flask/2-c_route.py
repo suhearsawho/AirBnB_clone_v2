849a68d3e6a2fbae34804e19472b10407b54f3e9
susansu <susansu.software@gmail.com>
2019-04-22 21:06:17 +0000

    changed permissions

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """This starts a Flask web application"""
-from flask import Flask, render_template
+from flask import Flask
 
 
 def create_app(test_config=None):
@@ -21,22 +21,6 @@ def create_app(test_config=None):
     def c_page(text):
         """c page"""
         return 'C {}'.format(text.replace('_', ' '))
-
-    @app.route('/python', defaults={'text': 'is_cool'}, strict_slashes=False)
-    @app.route('/python/<text>', strict_slashes=False)
-    def python_page(text):
-        """Python page"""
-        return 'Python {}'.format(text.replace('_', ' '))
-
-    @app.route('/number/<int:n>', strict_slashes=False)
-    def number(n):
-        """number page"""
-        return str(n)
-
-    @app.route('/number_template/<int:n>', strict_slashes=False)
-    def number_template(n):
-        """number template page"""
-        return render_template('5-number.html', n=n)
 
     return app
 
