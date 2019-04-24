5c3c971c79f21c5e1b7a0f902ad3951cbf537101
susansu <susansu.software@gmail.com>
2019-04-24 16:47:20 +0000

    trying to find error

--- models/state.py
+++ models/state.py
@@ -28,8 +28,9 @@ class State(BaseModel, Base):
         @property
         def cities(self):
             """Returns the list of City instances with equal state_id"""
-            from models import storage
-            cities = storage.all(City)
-            self.__cities = [city for city in cities.values()
-                             if city.state_id == self.id]
-            return self.__cities
+            pass
+#            from models import storage
+#           cities = storage.all(City)
+#           self.__cities = [city for city in cities.values()
+#                            if city.state_id == self.id]
+#           return self.__cities
